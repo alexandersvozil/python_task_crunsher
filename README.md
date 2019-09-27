9473a3e4b2542d2c6a254a1e7b66a52a33c80e69
Alexander Svozil <alexander.svozil@gmail.com>
2019-09-27 17:06:15 +0200

    simplified script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # python_task_crunsher
-Easy script to execute a list of shell commands with the maximum number of processors.
-
-Example: python3 task_crunsher example/task_cruncher_test
+Easy script to execute a list of shell commands with given amount of processors.
+USAGE: python3 task_crunsher file_of_shells_commands nr_of_processors
+Example: python3 task_crunsher example/task_cruncher_test 2
 
 Inspired by https://github.com/schulzchristian/task_crunsher
--- task_crunsher.py
+++ task_crunsher.py
@@ -1,40 +1,30 @@
 #Process level parallelism for shell commands
 import sys
 import subprocess as sp
-import multiprocessing as mp
 import concurrent.futures
 
-def read_file(in_file, queue):
+def read_file(in_file):
     """Defines the work unit on an input file"""
+    commands = [];
     for line in open(in_file):
         if line[0] == '#' or line[0] == '\n':
             continue
         print (line)
-        queue.put(line)
-    queue.put(None)
-    return 0;
+        commands.append(line);
+    return commands;
 
-def do_work(queue):
-    while True:
-        command = queue.get()
-        if (command is None):
-            break;
-        print ("processing: ", command, " on processor: ");#, multiprocessing.current_process().name)
+def do_work(command):
+        print ("processing: ", command);
         sp.check_output(command, shell=True);
-    return 0;
 
 if __name__ == '__main__':
     #Specify files to be worked with typical shell syntax and glob module
     tasks = sys.argv[1]
-    queue = mp.Queue()
-    
-    read_file(tasks, queue);
-
-    #Set up the parallel task pool to use all available processors
-    count = mp.cpu_count()
-    print ("cpu_count: ", count)
+    workers = sys.argv[2]
+    commands = read_file(tasks);
+    print (commands);
 
     #Run the jobs
-    with concurrent.futures.ThreadPoolExecutor(max_workers=count) as executor:     
-        executor.submit(do_work,queue) 
+    with concurrent.futures.ThreadPoolExecutor(max_workers=int(workers)) as executor:          
+        executor.map(do_work,commands);
 
